38f01019e658e4dd3270e03d49d32fd316d18ae1
Luca Bello <luca.bello@canonical.com>
2025-01-24 15:18:19 +0100

    add live model probe

--- src/main.py
+++ src/main.py
@@ -4,14 +4,18 @@ import logging
 import sys
 import tempfile
 from pathlib import Path
-from typing import Annotated, List, Optional
+from typing import Annotated, Any, Dict, List, Optional
 
 import sh
 import typer
+import yaml
+
 from rich.console import Console
 from rich.logging import RichHandler
 
 from fetcher import Probe, fetch_probes
+
+# pyright: reportAttributeAccessIssue=false
 
 logging.basicConfig(level=logging.WARN, handlers=[RichHandler()])
 log = logging.getLogger(__name__)
@@ -20,23 +24,48 @@ app = typer.Typer()
 console = Console()
 
 
+def _read_file(filename: str):
+    """Read a file into a string."""
+    with open(filename, "r") as f:
+        return f.read()
+
+
+def _get_model_data(model: str, probe_category: str) -> str:
+    """Get data from a live model according to the type of probe."""
+    match probe_category:
+        case "status":
+            return sh.juju.status(model=model, format="yaml", _tty_out=False)
+        case "bundle":
+            return sh.juju("export-bundle", model=model, format="yaml", _tty_out=False)
+        case "show-unit":
+            units: List[str] = []
+            show_units: Dict[str, Any] = {}  # List of show-unit results in dictionary form
+            juju_status = yaml.safe_load(
+                sh.juju.status(model=model, format="yaml", _tty_out=False)
+            )
+            for app in juju_status["applications"]:
+                units.extend(juju_status["applications"][app]["units"].keys())
+            for unit in units:
+                show_unit = yaml.safe_load(
+                    sh.juju("show-unit", unit, model=model, format="yaml", _tty_out=False)
+                )
+                show_units.update(show_unit)
+            return yaml.dump(show_units)
+
+
 @app.command()
 def check(
-    # charms: Annotated[
-    #     List[str],
-    #     typer.Option("--charm", "-c", help="Name of a charm whose probes to execute"),
-    # ] = [],
     probe_uris: Annotated[
         List[str],
         typer.Option("--probe", "-p", help="URI of a probe containing probes to execute." ""),
     ] = [],
-    models: Annotated[
-        List[str],
+    model: Annotated[
+        Optional[str],
         typer.Option("--model", "-m", help="Model on which to run live checks"),
-    ] = [],
+    ] = None,
     juju_status_file: Annotated[
         Optional[str],
-        typer.Option("--status", help="Juju status in a .json format"),
+        typer.Option("--status", help="Juju status in a .yaml format"),
     ] = None,
     juju_bundle_file: Annotated[
         Optional[str],
@@ -64,13 +93,16 @@ def check(
             for probe in probes:
                 log.info(f"Running probe {probe}")
                 if probe.is_show_unit():
-                    if not juju_show_unit:
-                        raise Exception("You didn't supply a juju show-unit.")
+                    input_data = juju_show_unit
+                    if model and not input_data:
+                        input_data = _get_model_data(model, "show-unit")
+                    if not input_data:
+                        raise Exception("You didn't supply a juju show-unit or a live model.")
                     try:
-                        sh.python(probe.path, _in=juju_show_unit)  # type: ignore
-                        console.print(f"{probe.name} succeeded :tada:")
+                        sh.python(probe.path, _in=input_data)
+                        console.print(f":green_circle: {probe.name} succeeded")
                     except sh.ErrorReturnCode:
-                        console.print(f"{probe.name} failed :sob:")
+                        console.print(f":red_circle: {probe.name} failed")
 
 
 @app.command()
