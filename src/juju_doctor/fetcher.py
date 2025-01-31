629837b5315ae654b2fafb9bb0640d7a3eef2256
Michael Thamm <mike.thamm@canonical.com>
2025-01-31 01:23:33 -0500

    charm protocol - before Fetcher class

--- src/juju_doctor/fetcher.py
+++ src/juju_doctor/fetcher.py
@@ -9,7 +9,7 @@ from enum import Enum
 from pathlib import Path
 from typing import List, Optional
 from urllib.error import URLError
-from urllib.parse import urlparse
+from urllib.parse import ParseResult, urlparse
 
 import fsspec
 
@@ -39,12 +39,26 @@ class Probe:
         return self.category == category
 
 
+def _get_filesystem(filesystem: fsspec.filesystem, path: Path, local_path: Path, parsed_uri: ParseResult):
+    try:
+        # If path ends with a "/", it will be assumed to be a directory
+        # Can submit a list of paths, which may be glob-patterns and will be expanded.
+        # https://github.com/fsspec/filesystem_spec/blob/master/docs/source/copying.rst
+        filesystem.get(path.as_posix(), local_path.as_posix(), recursive=True, auto_mkdir=True)
+    except FileNotFoundError as e:
+        log.warn(f"file not found when attempting to copy {path.as_posix()} to {local_path.as_posix()}: {e}")
+        raise
+
+# TODO turn this into a class
 def fetch_probes(uri: str, destination: Path) -> List[Probe]:
     """Fetch probes from a source to a local directory.
 
     Args:
         uri: a URI to a probe living somewhere.
-            Currently only supports 'github://' with Terraform notation and 'file://'.
+            Currently only supports:
+            - 'github://' with Terraform notation
+            - 'file://'
+            - 'charm://' for fetching from charm(-k8s)-operator remotes
         destination: the folder where to save the probes.
     """
     probes = []
@@ -52,6 +66,11 @@ def fetch_probes(uri: str, destination: Path) -> List[Probe]:
     probe_path = parsed_uri.netloc + parsed_uri.path
     subfolder = probe_path.replace("/", "_")
     local_path = destination / subfolder
+    # Extract the branch name if present
+    if parsed_uri.query:
+        branch = parsed_uri.query
+    else:
+        branch = "main"
 
     match parsed_uri.scheme:
         case "file":
@@ -63,34 +82,49 @@ def fetch_probes(uri: str, destination: Path) -> List[Probe]:
                 org_and_repo, path = probe_path.split("//")
                 path = Path(path)
                 org, repo = org_and_repo.split("/")
-                # Extract the branch name if present
-                if parsed_uri.query:
-                    branch = parsed_uri.query
-                else:
-                    branch = "main"
             except ValueError:
                 raise URLError(
                     f"Invalid URL format: {uri}. Use '//' to define 1 sub-directory "
                     "and specify at most 1 branch."
                 )
-            filesystem = fsspec.filesystem(
-                protocol="github", org=org, repo=repo, sha=f"refs/heads/{branch}"
-            )
+            filesystem = fsspec.filesystem(protocol="github", org=org, repo=repo, sha=f"refs/heads/{branch}")
+        case "charm":
+            # Extract the charm names from the URI, stripping the leading "/"
+            charms = [parsed_uri.netloc] + parsed_uri.path[1:].split("/")
+            # Get each filesystem from the GitHub repo named charm(-k8s)-operator format
+            for charm in charms:
+                filesystem = fsspec.filesystem(
+                    protocol="github", org="canonical", repo=f"{charm}-operator", sha=f"refs/heads/{branch}"
+                )
+                probes_path = Path("probes")
+                try:
+                    _get_filesystem(filesystem, probes_path, local_path, parsed_uri)
+                except FileNotFoundError:
+                    pass
+
+                raise NotImplementedError
+                probe = Probe(
+                    name=probes_path.relative_to(destination).as_posix(),
+                    category="from-gh",
+                    uri=uri,
+                    original_path=Path(path),
+                    local_path=probe_path,
+                )
+            return probes
         case _:
             raise NotImplementedError
 
-    # If path ends with a "/", it will be assumed to be a directory
-    # Can submit a list of paths, which may be glob-patterns and will be expanded.
-    # https://github.com/fsspec/filesystem_spec/blob/master/docs/source/copying.rst
-    filesystem.get(path.as_posix(), local_path.as_posix(), recursive=True, auto_mkdir=True)
+    try:
+        _get_filesystem(filesystem, path, local_path, parsed_uri)
+    except FileNotFoundError:
+        return probes
+
     log.info(f"copying {path.as_posix()} to {local_path.as_posix()} recursively")
 
     if filesystem.isfile(path.as_posix()):
         probe_files = [local_path]
     else:
-        probe_files: List[Path] = [
-            f for f in local_path.rglob("*") if f.as_posix().endswith(".py")
-        ]
+        probe_files: List[Path] = [f for f in local_path.rglob("*") if f.as_posix().endswith(".py")]
 
     for probe_path in probe_files:
         # NOTE: very naive category recognition
