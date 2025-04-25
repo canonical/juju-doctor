5bc135895b57cd72b4c34f36205aad5af4967df4
Michael Thamm <mike.thamm@canonical.com>
2025-04-25 19:36:23 +0100

    fix: unit test for gh dir probes (#35)
    * fix: unit test for gh dir probes

--- tests/unit/probe_uri_parsing/test_github_protocol.py
+++ tests/unit/probe_uri_parsing/test_github_protocol.py
@@ -30,17 +30,9 @@ def test_parse_dir():
     with tempfile.TemporaryDirectory() as tmpdir:
         # WHEN the probes are fetched to a local filesystem
         probes = Probe.from_url(url=probe_url, probes_root=Path(tmpdir))
-        # THEN 2 probe exists
-        assert len(probes) == 2
-        passing_probe = [probe for probe in probes if "passing.py" in probe.name][0]
-        failing_probe = [probe for probe in probes if "failing.py" in probe.name][0]
-        # AND the Probe was correctly parsed as passing
-        assert (
-            passing_probe.name == "canonical_juju-doctor__tests_resources_probes_python/passing.py"
-        )
-        assert passing_probe.path == Path(tmpdir) / passing_probe.name
-        # AND the Probe was correctly parsed as failing
-        assert (
-            failing_probe.name == "canonical_juju-doctor__tests_resources_probes_python/failing.py"
-        )
-        assert failing_probe.path == Path(tmpdir) / failing_probe.name
+        # THEN each Probe is correctly parsed
+        for probe in probes:
+            file_name = probe.name.split("/")[-1]
+            url_flattened = f"{'tests/resources/probes/python'.replace('/', '_')}/{file_name}"
+            assert probe.name == f"canonical_juju-doctor__{url_flattened}"
+            assert probe.path == Path(tmpdir) / probe.name
