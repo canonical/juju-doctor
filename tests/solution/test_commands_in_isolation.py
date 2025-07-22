f13cb8b46f5376b10335acc23e3604624782715c
Michael Thamm <mike.thamm@canonical.com>
2025-07-21 23:42:01 -0400

    chore: Try out conftest.py for getting solution test isolation to work

--- tests/solution/test_commands_in_isolation.py
+++ tests/solution/test_commands_in_isolation.py
@@ -1,37 +1,27 @@
 import json
 
-import pytest
-from typer.testing import CliRunner
-
-from juju_doctor.main import app
-
-
-@pytest.fixture(scope="function")
-def runner():
-    return CliRunner()
-
-
-@pytest.fixture(scope="function")
-def app_fixture():
-    return app
 
 # https://github.com/fastapi/typer/discussions/1259
 def test_1(runner, app_fixture):
     test_args = [
         "check",
         "--format=json",
-        "--probe=file://tests/resources/probes/python/mixed.py",
+        "--probe=file://tests/resources/probes/python/passing.py",
         "--status=tests/resources/artifacts/status.yaml",
+        "--bundle=tests/resources/artifacts/bundle.yaml",
+        "--show-unit=tests/resources/artifacts/show-unit.yaml",
     ]
     result = runner.invoke(app_fixture, test_args)
-    assert json.loads(result.stdout)["Results"]["children"] == 1 # Now has len == 1
+    assert len(json.loads(result.stdout)["Results"]["children"]) == 1 # Now has len == 1
 
 def test_2(runner, app_fixture):
     test_args = [
         "check",
         "--format=json",
-        "--probe=file://tests/resources/probes/python/mixed.py",
+        "--probe=file://tests/resources/probes/python/failing.py",
         "--status=tests/resources/artifacts/status.yaml",
+        "--bundle=tests/resources/artifacts/bundle.yaml",
+        "--show-unit=tests/resources/artifacts/show-unit.yaml",
     ]
     res = runner.invoke(app_fixture, test_args)
-    assert json.loads(res.stdout)["Results"]["children"] == 1 # Now has len == 2
+    assert len(json.loads(res.stdout)["Results"]["children"]) == 1 # Now has len == 2
